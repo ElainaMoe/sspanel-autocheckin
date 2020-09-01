4f306e03ca9644739001c6caf66564154cd0430d
Secret <i@wangmao.me>
2020-09-01 11:36:38 +0800

    配置 CI

--- ssp-autocheckin.sh
+++ ssp-autocheckin.sh
@@ -4,36 +4,39 @@ PATH="/usr/local/bin:/usr/bin:/bin"
 
 ENV_PATH="$(dirname $0)/.env"
 
-if [ ! -f ${ENV_PATH} ]; then
-    echo "配置不存在，请复制 env.example 到 .env 并修改配置" && exit 1
+if [ -f ${ENV_PATH} ]; then
+    source ${ENV_PATH}
 fi
 
-source ${ENV_PATH}
+if [ "${WORK_DOMAIN}" == "" ] || [ "${WORK_USERNAME}" == "" ] || [ "${WORK_PASSWD}" == "" ]; then
+    echo "环境常量未配置，请正确配置 DOMAIN、USERNAME 和 PASSWD 值" && exit 1
+fi
 
-# Check jq command exist
-if [ $(command -v jq) == "" ] 
-then
+if [ $(command -v jq) == "" ]; then
     echo "依赖缺失: jq，查看 https://github.com/isecret/sspanel-autocheckin/blob/master/README.md 安装" && exit 1
 fi
 
 COOKIE_PATH="./.ss-autocheckin.cook"
 
-login=$(curl "${DOMAIN}/auth/login" -d "email=${USERNAME}&passwd=${PASSWD}&code=" -c ${COOKIE_PATH} -L -k -s)
+login=$(curl "${WORK_DOMAIN}/auth/login" -d "email=${WORK_USERNAME}&passwd=${WORK_PASSWD}&code=" -c ${COOKIE_PATH} -L -k -s)
 
 date=$(date '+%Y-%m-%d %H:%M:%S')
 login_status=$(echo ${login} | jq '.msg')
 
+if [ "${login_status}" == "" ]; then
+    login_status='"登录失败"'
+fi
+
 echo "[${date}] ${login_status}"
 
-checkin=$(curl -k -s -d "" -b ${COOKIE_PATH} "${DOMAIN}/user/checkin")
+checkin=$(curl -k -s -d "" -b ${COOKIE_PATH} "${WORK_DOMAIN}/user/checkin")
 
 rm -rf ${COOKIE_PATH}
 
 date=$(date '+%Y-%m-%d %H:%M:%S')
 checkin_status=$(echo ${checkin} | jq '.msg')
 
-if [ "${checkin_status}" == "" ]
-then
+if [ "${checkin_status}" == "" ]; then
     checkin_status='"签到失败"'
 fi
 
